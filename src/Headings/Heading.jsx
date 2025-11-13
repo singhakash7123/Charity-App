c016021869ff7e9c555e71ef4a79153325a6ea4d
singhakash7123 <vivek414viveksingh@gmail.com>
2025-11-13 12:51:26 +0530

    done

--- src/Headings/Heading.jsx
+++ src/Headings/Heading.jsx
@@ -4,7 +4,7 @@ const Heading = ({content}) => {
   return (
     <>
     
-    <div className='text-5xl lg:text-6xl font-extrabold text-gray-700 lg:mt-10 w-fit'>
+    <div className='text-5xl lg:text-6xl font-extrabold text-gray-700 mt-10 w-fit'>
         {content}
         <div className='w-9 h-1 bg-orange-500 mt-6' ></div>
         </div>
