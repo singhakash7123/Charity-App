b77f3f931a92e2241f38bb6ecb7b7cd77dcb962f
singhakash7123 <vivek414viveksingh@gmail.com>
2025-11-29 13:26:08 +0530

    Updates

---
+++ assets/ScrollToTop-B4hPbZYQ.js
@@ -0,0 +1,1 @@
+import{u as t,r}from"./index-C4B-ULr1.js";function a(){const o=t();return r.useEffect(()=>{window.scrollTo({top:0,behavior:"smooth"})},[o]),null}export{a as S};
--- assets/ScrollToTop-Nsa_JVrw.js
+++
@@ -1,1 +0,0 @@
-import{u as t,r}from"./index-C8NmHCXX.js";function a(){const o=t();return r.useEffect(()=>{window.scrollTo({top:0,behavior:"smooth"})},[o]),null}export{a as S};
