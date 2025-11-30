f013ea4be1ecc5cbcb54f9511b61cce58239bb53
Kamal <kamalsinghpatel6013@gmail.com>
2025-11-30 19:17:18 +0530

    Cleaned up code: removed unnecessary comments

--- backened/server.js
+++ backened/server.js
@@ -15,18 +15,12 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api/payment", paymentRoutes);
-// Create Razorpay instance
-// export const razorpay = new Razorpay({
-//   key_id: process.env.RAZORPAY_KEY_ID,
-//   key_secret: process.env.RAZORPAY_KEY_SECRET,
-// });
 
-// Temporary route (you already had this)
 app.get("/", (req, res) => {
   res.redirect("http://localhost:5173");
 });
 
-// We will add payment routes here step-by-step
+
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server running at http://localhost:${process.env.PORT || 5000}`);
