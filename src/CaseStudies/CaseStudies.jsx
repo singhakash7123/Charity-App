0714ed70f66bb000fed6540735fc94e25311dea8
singhakash7123 <vivek414viveksingh@gmail.com>
2025-12-01 18:58:46 +0530

    Done

--- src/CaseStudies/CaseStudies.jsx
+++ src/CaseStudies/CaseStudies.jsx
@@ -9,12 +9,7 @@ const CaseStudies = () => {
          
         </div>
         <div className='max-w-[1400px] w-full mx-auto py-20 px-5 lg:px-0 '>
-         <div className='font-semibold lg:text-lg flex items-center justify-center gap-x-6 lg:gap-x-18'>
-            <button>ALL</button>
-            <button>FOOD</button>
-            <button>HELP</button>
-            <button>PEACE</button>
-         </div>
+         
          <div className='flex  items-center justify-center flex-wrap gap-x-10 gap-y-10 mt-15 '>
             <div className='max-w-[440px] w-full ' >
                <img className='w-full aspect-4/3 bg-blue-500' src="https://plus.unsplash.com/premium_photo-1683140538884-07fb31428ca6?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZG9uYXRpb258ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=600" alt="" />
